f86272485b13e8fbcb620aff4866125680272804
KiyohimeChyan <dongli_0607@126.com>
2023-05-17 21:04:14 +0100

    Add TimeCount Mechanic
    Add TimeCount Mechanic

--- Assets/Scripts/SceneLoad/PrototypeToFeedback.cs
+++ Assets/Scripts/SceneLoad/PrototypeToFeedback.cs
@@ -7,9 +7,22 @@ using UnityEngine.UI;
 
 public class PrototypeToFeedback : MonoBehaviour
 {
+    public bool isLevel3;
     public GameObject gameResetPanel;
     public TMP_Text result;
     public Button selectedButton;
+    public TMP_Text timeCount;
+    float usedTime;
+
+    private void Update()
+    {
+        if (isLevel3)
+        {
+            usedTime += Time.deltaTime;
+            timeCount.text = usedTime.ToString("f2");
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         gameResetPanel.SetActive(true);
